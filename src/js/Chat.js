7c9a5a262810b82fdb0881fd823358b6395b768c
saetovdinar <saetonskie@mail.ru>
2024-08-12 10:59:14 +0300

    final version

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -4,40 +4,47 @@ import ChatApi from "./ChatApi";
 
 export default class Chat {
     constructor(container) {
+        //контейнер приложения
         this.container = container;
-        
+        //кнопка отпарвка сообщений
         this.submitBtn = document.querySelector('.submit');
+        //инпут чата
         this.text = document.querySelector('.input');
+        //контейнер чат
         this.chat = document.querySelector('.chat');
-
+        //кнопка загрузки файлов с компьютера
         this.fileLoadBtn = document.querySelector('.overlap');
         this.fileLoad = document.querySelector('.file_load');
-
+        //кнопка старта и финиша видеозаписи
         this.recordVideoBtn = document.querySelector('.video_record');
         this.stopVideoBtn = document.querySelector('.video_stop');
-
+        //кнопка старта и финиша аудиозаписи
         this.recordAudioBtn = document.querySelector('.audio_record');
         this.stopAudioBtn = document.querySelector('.audio_stop');
+        //регистрация всех событий
         this.eventRegister();
 
-
+        //фиксация url 
         this.api = new ChatApi('http://localhost:7070')
-
+        //отрисовка сообщений из БД
         this.renderChatStorage();
 
        
     }
 
-    
+    //реализует одноименную функцию.
     DragNDrop() {
         this.container.addEventListener('dragover', (event) => {
           event.preventDefault();  
         })
         this.container.addEventListener('drop', (event) => {
             event.preventDefault(); 
+            //данные файла
             const file = event.dataTransfer.files && event.dataTransfer.files[0];
+            //перемещение данных во временное хранилище и генерация URL
             const url = URL.createObjectURL(file);
             let contentCont;
+            //создание контейнеров для медиа
             if(file.type.indexOf('image') != -1) {
                 contentCont = document.createElement('img');
                 contentCont.classList.add('content_cont');
@@ -56,6 +63,7 @@ export default class Chat {
                 contentCont.src = url;
                 contentCont.controls = true;
             }
+            //добавление их в чат
             const postCont = document.createElement('div');
             postCont.classList.add('post_cont');
             postCont.append(contentCont);
@@ -65,16 +73,17 @@ export default class Chat {
            
           })
     }
+    // релизует ленивую загрузку.
     lazyLoad() {
         this.chat.addEventListener('scroll' , () => {
-            
+            //проверка границы скрола пользователем
             if(this.chat.scrollTop === 0) {
-               
+                //отрисовка 10 сообщение при достижении верхней границы чата
                 this.api.getChat()
                 .then(data => {
                     data.forEach(msg => {
                
-        
+                        //проверка наличия медиа-ссылок в сообщении
                         if(msg.message.indexOf('http') != -1 && msg.message.indexOf('blob') != -1) {
                             let contentCont;
                             if(msg.type === 'image') {
@@ -99,7 +108,7 @@ export default class Chat {
                             return;
                         }
         
-        
+                        //проверка наличия ссылок в сообщении
                         if(msg.message.indexOf('http') != -1) {
                             const linkTag = document.createElement('a');
                             linkTag.href = msg.message;
@@ -110,7 +119,7 @@ export default class Chat {
                             this.chat.prepend(postCont)
                             return;
                         }
-        
+                        //добавление сообщения в чат
                         const postCont = document.createElement('div');
                         postCont.classList.add('post_cont');
                         postCont.append(msg.message);
@@ -124,12 +133,14 @@ export default class Chat {
        
        
     }
+    //рендер сообщений из БД.
     renderChatStorage() {
+        //запрос истории чата
         this.api.getChat()
         .then(data => {
             data.forEach(msg => {
        
-
+                //проверка наличия медиа-ссылок в сообщениика
                 if(msg.message.indexOf('http') != -1 && msg.message.indexOf('blob') != -1) {
                     let contentCont;
                     if(msg.type === 'image') {
@@ -154,7 +165,7 @@ export default class Chat {
                     return;
                 }
 
-
+                //проверка наличия ссылок в сообщении
                 if(msg.message.indexOf('http') != -1) {
                     const linkTag = document.createElement('a');
                     linkTag.href = msg.message;
@@ -165,19 +176,20 @@ export default class Chat {
                     this.chat.prepend(postCont)
                     return;
                 }
-
+                //добавление в чат
                 const postCont = document.createElement('div');
                 postCont.classList.add('post_cont');
                 postCont.append(msg.message);
                 this.chat.prepend(postCont)
             })
+            //смещение скролла на высоту сообщения
             this.chat.scrollTop += this.chat.scrollHeight;
         })
     }
 
-
+    //отправка геолокации.
     postGeo(postCont) {
-        
+        //добавление геопозиции к сообщению
         const geoCont = document.createElement('div');
         geoCont.classList.add('geo_cont');
         navigator.geolocation.getCurrentPosition((data) => {
@@ -189,7 +201,7 @@ export default class Chat {
         
         
     }
- 
+    //контейнер для всех собтий.
     eventRegister() {
         this.videoPost();
         this.audioPost();
@@ -198,23 +210,25 @@ export default class Chat {
         this.lazyLoad();
         this.DragNDrop();
     }
+    //загрузка файлов, реализация скачивания фалов на компьютер и с компьютера и отправка в БД.
     loadFile() {
+        //отслживание загрузки файла в чат
         this.fileLoad.addEventListener('change', (event) => {
    
-            
+            //получение данных
             const file = this.fileLoad.files && this.fileLoad.files[0];
 
             if(!file) return; 
 
 
             const body = new FormData();
-           
+            //перемещение данных во временное хранилище и генерация URL
             const url = URL.createObjectURL(file);
 
             
            
             let contentCont;
-
+            //создание елемента узла соответсвенно типу медиа
             if(file.type.indexOf('image') != -1) {
                 contentCont = document.createElement('img');
                 contentCont.classList.add('content_cont');
@@ -238,14 +252,14 @@ export default class Chat {
             contentCont.src = url;
 
            
-
+            //отправка сообщения в БД
             this.api.add(body)
             
            
-           
+            //отрисовка в чат
             const postCont = document.createElement('div');
             postCont.classList.add('post_cont');
-            
+            //генерация кнопки загрузки файла на компьютер
             const downloadBtn = document.createElement('div');
             downloadBtn.classList.add('download_btn');
 
@@ -268,8 +282,9 @@ export default class Chat {
        
 
     )}
-
+    // создание, настройка узлов - элементов для проигрывателей в браузере
     createPLayer(type) {
+
         const player = document.createElement(type);
         player.classList.add(type);
         player.setAttribute('controls', 'controls');
@@ -281,7 +296,7 @@ export default class Chat {
 
         return [postCont, player];
     }
-     
+    //запись медиа потока.
     recorder(media, chunk, player) {
                 const recorder = new MediaRecorder(media);
                 
@@ -300,7 +315,7 @@ export default class Chat {
                 recorder.start();
                 return recorder;
     }
-       
+    //создание элемента кнопки скачивания на компьютер медиа файлов.
     createDownloadBtn(chunk, format) {
             const downloadBtn = document.createElement('div');
             downloadBtn.classList.add('download_btn');
@@ -321,15 +336,16 @@ export default class Chat {
             });
             return downloadBtn;
     }
-
+    //событие старта и финиша записи видео и отправка в чат и БД.
     videoPost() {
             
             
         this.recordVideoBtn.addEventListener('click', async (event) => {
             event.preventDefault();
+            //изменение иконки старта/финиша записи
             this.recordVideoBtn.style.display = 'none';
             this.stopVideoBtn.style.display = 'block';
-            
+            //запрос на запись онлайн
             const media = await navigator.mediaDevices.getUserMedia({
                 video: true,
         });
@@ -339,21 +355,22 @@ export default class Chat {
           
            
             this.currentPost = postCont;
-
+            //геолокация
             this.postGeo(this.currentPost);
-
+            //массив для данных записи
             const chunk = [];
             const record = this.recorder(media, chunk,player);
-
+            //кнопка скачивания
             const downloadBtn = this.createDownloadBtn(chunk, 'video.mp4');
 
             postCont.append(downloadBtn);
             this.chat.append(postCont);
-
+            //смещение скролла на высоту сообщения
             this.chat.scrollTop += postCont.offsetHeight ;
-           
+           //финиш записи
             this.stopVideoBtn.addEventListener('click', async (event) => {
                 event.preventDefault();
+                 //изменение иконки старта/финиша записи
                 this.recordVideoBtn.style.display = 'block';
                 this.stopVideoBtn.style.display = 'none';
                 record.stop();
@@ -362,7 +379,7 @@ export default class Chat {
             })
     })
     }
-
+    //событие старта и финиша записи аудио и отправка в чат и БД.
     audioPost() {
         
        
@@ -413,6 +430,7 @@ export default class Chat {
         })
     })
     }
+    //отправка текстовых сообщений в чат и БД
 
     textPost() {
       
@@ -421,7 +439,7 @@ export default class Chat {
             event.preventDefault(); 
 
             const textValue = this.text.value;
-
+            //проверка на команду для отправки погоды
             if(textValue.indexOf('@weather') != -1) {
                 
             
@@ -444,18 +462,22 @@ export default class Chat {
                 return;
               
             }
+            //проверка на команду для отправки уведомлений и напоминаний
+            //формат ввода данных строго: @schedule: 2024-08-12 10:46 «Последний день лета»
 
             if(textValue.indexOf('@schedule') != -1) {
-                
+                //разбор строки по пробелам
                 const inputSlice = textValue.split(' ');
                 let [shedule,  day, time, ...text] = inputSlice;
-
+                //текущее время
                 const currentDate = Date.now();
+                //перевод будущей даты в ms
                 const appDate =  Date.parse(day + 'T' + time);
                 const textForNoti = text.join(' ');
                 function  showNoti() {
                  new Notification(textForNoti);  
                }
+               //таймер на уведомление по разнице времени
                setTimeout(showNoti, appDate-currentDate );
                 
                
@@ -463,17 +485,17 @@ export default class Chat {
                 return;
               
             }
-           
+           //отправка сообщения в БД
             const body = new FormData();
             body.append('message', textValue);
             body.append('type', 'text');
             this.api.add(body)
-           
+            
             const postCont = document.createElement('div');
             postCont.classList.add('post_cont');
             postCont.append(textValue);
             this.chat.append(postCont);
-            
+            //смещение скролла на высоту сообщения
             this.chat.scrollTop += postCont.offsetHeight 
       
            
